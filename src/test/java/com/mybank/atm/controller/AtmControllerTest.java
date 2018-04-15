fb4604813b7e8744aa8f86c5f788bc8618b282e0
Brian Reynolds <brian.e.reynolds@outlook.com>
2018-04-15 23:58:59 +0100

    improve coverage

--- src/test/java/com/mybank/atm/controller/AtmControllerTest.java
+++ src/test/java/com/mybank/atm/controller/AtmControllerTest.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = AtmApplication.class)
 @AutoConfigureMockMvc
+@Transactional
 public class AtmControllerTest {
 
     @Autowired
@@ -29,7 +32,7 @@ public class AtmControllerTest {
     public void testGetBalance() throws Exception {
         Long testAccountNum = 100100013L;
         String testPin = "9923";
-        String url = getUrl(testAccountNum, testPin);
+        String url = getBalanceUri(testAccountNum, testPin);
 
         this.mockMvc.perform(get(url))
                 .andExpect(status().isOk())
@@ -43,7 +46,7 @@ public class AtmControllerTest {
     public void testInvalidAccountNum() throws Exception {
         Long testAccountNum = 11L;
         String testPin = "0000";
-        String url = getUrl(testAccountNum, testPin);
+        String url = getBalanceUri(testAccountNum, testPin);
 
         this.mockMvc.perform(get(url))
                 .andExpect(status().isOk())
@@ -56,7 +59,7 @@ public class AtmControllerTest {
     public void testIncorrectPin() throws Exception {
         Long testAccountNum = 443457424L;
         String testPin = "0000";
-        String url = getUrl(testAccountNum, testPin);
+        String url = getBalanceUri(testAccountNum, testPin);
 
         this.mockMvc.perform(get(url))
                 .andExpect(status().isOk())
@@ -65,8 +68,99 @@ public class AtmControllerTest {
                 .andExpect(jsonPath("$.message", is(ErrorMessages.INVALID_PIN)));
     }
 
-    private String getUrl(Long accountNum, String pin) {
-        return String.format("/balance/%s/%s", accountNum, pin);
+    @Test
+    public void testWithdrawFundsBasic() throws Exception {
+        Long testAccountNum = 443457424L;
+        String testPin = "5623";
+        String url = getWithdrawUri(testAccountNum, testPin, "300");
 
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.amount", is(300)))
+                .andExpect(jsonPath("$.cashMap.fifty", is(6)))
+                .andExpect(jsonPath("$.cashMap.twenty", is(0)))
+                .andExpect(jsonPath("$.cashMap.ten", is(0)))
+                .andExpect(jsonPath("$.cashMap.five", is(0)))
+                .andExpect(jsonPath("$.accountResource.balance", is(54.54)))
+                .andExpect(jsonPath("$.accountResource.maxWithdrawal", is(554.54)));
+    }
+
+    @Test
+    public void testWithdrawFundsOverdraft() throws Exception {
+        Long testAccountNum = 443457424L;
+        String testPin = "5623";
+        String url = getWithdrawUri(testAccountNum, testPin, "845");
+
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.amount", is(845)))
+                .andExpect(jsonPath("$.cashMap.fifty", is(16)))
+                .andExpect(jsonPath("$.cashMap.twenty", is(2)))
+                .andExpect(jsonPath("$.cashMap.ten", is(0)))
+                .andExpect(jsonPath("$.cashMap.five", is(1)))
+                .andExpect(jsonPath("$.accountResource.balance", is(0)))
+                .andExpect(jsonPath("$.accountResource.maxWithdrawal", is(9.54)));
+    }
+
+    @Test
+    public void testWithdrawFundsUntilBroke() throws Exception {
+        Long testAccountNum = 494911101L ;
+        String testPin = "4425";
+        String url = getWithdrawUri(testAccountNum, testPin, "95");
+
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.amount", is(95)))
+                .andExpect(jsonPath("$.cashMap.fifty", is(1)))
+                .andExpect(jsonPath("$.cashMap.twenty", is(2)))
+                .andExpect(jsonPath("$.cashMap.ten", is(0)))
+                .andExpect(jsonPath("$.cashMap.five", is(1)))
+                .andExpect(jsonPath("$.accountResource.balance", is(5.00)))
+                .andExpect(jsonPath("$.accountResource.maxWithdrawal", is(405.00)));
+
+        url = getWithdrawUri(testAccountNum, testPin, "205");
+
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.amount", is(205)))
+                .andExpect(jsonPath("$.cashMap.fifty", is(4)))
+                .andExpect(jsonPath("$.cashMap.twenty", is(0)))
+                .andExpect(jsonPath("$.cashMap.ten", is(0)))
+                .andExpect(jsonPath("$.cashMap.five", is(1)))
+                .andExpect(jsonPath("$.accountResource.balance", is(0)))
+                .andExpect(jsonPath("$.accountResource.maxWithdrawal", is(200.00)));
+
+        url = getWithdrawUri(testAccountNum, testPin, "150");
+
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.amount", is(150)))
+                .andExpect(jsonPath("$.cashMap.fifty", is(3)))
+                .andExpect(jsonPath("$.cashMap.twenty", is(0)))
+                .andExpect(jsonPath("$.cashMap.ten", is(0)))
+                .andExpect(jsonPath("$.cashMap.five", is(0)))
+                .andExpect(jsonPath("$.accountResource.balance", is(0)))
+                .andExpect(jsonPath("$.accountResource.maxWithdrawal", is(50.00)));
+
+        url = getWithdrawUri(testAccountNum, testPin, "75");
+
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.code", is("A1002")))
+                .andExpect(jsonPath("$.message", is("Insufficient account balance")));
+    }
+
+    private String getBalanceUri(Long accountNum, String pin) {
+        return String.format("/balance/%s/%s", accountNum, pin);
+    }
+
+    private String getWithdrawUri(Long accountNum, String pin, String amount) {
+        return String.format("/withdraw/%s/%s/%s", accountNum, pin, amount);
     }
 }
